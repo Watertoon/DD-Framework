3d2f7341a7ee06d850187fa96ed9c07e9b9cce76
Michael <wmknudson9@gmail.com>
2022-11-24 12:47:27 -0800

    Add root heap check to HeapManager test

--- unit_tests/lib_dd_tests/source/mem/test_mem_heapmanager.cpp
+++ unit_tests/lib_dd_tests/source/mem/test_mem_heapmanager.cpp
@@ -16,9 +16,17 @@ TEST(HeapManagerInitialization) {
 
     /* Initialize System Manager */
     dd::sys::InitializeSystemManager();
-    
+
     /* Initialize and finalize heap manager */
     dd::mem::InitializeHeapManager(dd::util::Size32MB);
+
+    /* Find root heap */
+    dd::mem::Heap *heap = dd::mem::FindHeapByName("HeapManager::sRootHeap");
+    TEST_ASSERT(heap != nullptr);
+    TEST_ASSERT(::strcmp(heap->GetName(), "HeapManager::sRootHeap") == 0);
+
+    /* Finalize */
     dd::mem::FinalizeHeapManager();
+
     TEST_SUCCESS;
 }
